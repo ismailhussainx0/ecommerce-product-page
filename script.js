382e2aea81d9e8ce5e950fcdd5ece61c760c2860
ismail <ismail.hussainx0@gmail.com>
2026-06-12 00:53:35 +0500

    just added

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ let cartCounter = document.querySelector("#addToCartCounter");
 
 let counter = 0;
 
-cartBtn.addEventListener("click", function(){
+cartBtn.addEventListener("click", function () {
     counter++;
     cartCounter.innerText = counter;
+
+    cartBtn.innerText = "Added!";
+
+    setTimeout(function () {
+        cartBtn.innerText = "Add to Cart";
+    }, 1000)
 });
